0083e6c03b45e61f932827cbf1051d296a1fc5f7
DELENN\wa1go <darryl@hambus.org>
2018-06-10 11:29:16 -0500

    added generic rigbus to solution

---
+++ RigBus/IRig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RigBus
+{
+    public interface IRig
+    {
+        void setRigState(int rigId, RigState state);
+        void setMode(int rigId, string mode);
+        void setFreq(int rigId, int freq);
+        void setFreqA(int rigId, int freq);
+        void setFreqB(int rigId, int freq);
+        void setPitch(int rigId, int pitch);
+        void setRit(int rigId, string rit);
+        void setRitOffset(int rigId, int freq);
+        void setVfo(int rigId, string freq);
+        void set(int rigId, string xit);
+    }
+}
---
+++ RigBus/RigState.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RigBus
+{
+    public class RigState
+    {
+        public int Freq { get; set; }
+        public int FreqA { get; set; }
+        public int FreqB { get; set; }
+        public string Mode { get; set; }
+        public int Pitch { get; set; }
+        public string RigType { get; set; }
+        public string Rit { get; set; }
+        public int RitOffset { get; set; }
+        public string Status { get; set; }
+        public string StatusStr { get; set; }
+        public string Split { get; set; }
+        public string Tx { get; set; }
+        public string Vfo { get; set; }
+        public string Xit { get; set; }
+    }
+}
